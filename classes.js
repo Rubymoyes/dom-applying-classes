2f5982122045ae147a91c05743eea88230c49aa3
Rubymoyes <moyesruby@gmail,com>
2018-08-25 03:59:30 +1200

    invisible div made visible

--- classes.js
+++ classes.js
@@ -1,51 +1,47 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
-// https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
+
 document.addEventListener('DOMContentLoaded', start)
 
 function start () {
-  // The first example is done for you. Uncomment the line below and reload the browser.
-
-  one(), two (), three ()
-    
-  // Your turn! Create a new function called `two`, then call it from here.
+  one(), two (), three (), makeVisible ()
 }
 
 function one () {
-  // First, we have to find the element:
+  // Find the element
   var one = document.getElementById('one')
 
-  // Next, we apply a new CSS class to it:
+  // Apply a new CSS class to it:
   one.classList.add('blue')
 }
 
-// CREATE FUNCTION two HERE
+// Function 2
 
 function two () {
-  // First, we have to find the element:
   var two = document.getElementById('two')
-
-  // Next, we apply a new CSS class to it:
   two.classList.add('green')
 }
 
-
-
-// CREATE FUNCTION three HERE
+// Function 3
 
 function three () {
-  // First, we have to find the element:
   var three = document.getElementById('three')
-
-  // Next, we apply a new CSS class to it:
   three.classList.add('purple')
 }
 
+// makeVisible
 
+function makeVisible () {
+  var makeVisible = document.getElementsByClassName('invisible')[0]
+  makeVisible.classList.remove('invisible')[0]
+  makeVisible.classList.add('visible')[0]
+}
 
+/* At first, I wasn't sure why my code wasn't working for this. I did look up some resources
+on how to make it work. I was doing this:
+  var makeVisible = document.getElementsByClassName('invisible')
+  makeVisible.classList.remove('invisible')
+  makeVisible.classList.add('visible')
+Just a quick question, why is adding the [0] essential to making it work? :)
+  over and out! 
+  */
 
-
-// CREATE FUNCTION makeVisible HERE
-
-
-
+  
